3c363839f760d3832db8ec2eef144bd50911a06d
Asventi <jarnacpierreoff@gmail.com>
2023-08-11 00:10:41 +0200

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asventi/autopine_alerts
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+package autopine_alerts
+
+import (
+	"fmt"
+)
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hello, %s", name)
+	return message
+}
